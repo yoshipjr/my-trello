f7fd38d3b3f5090cf4c667015521f331c0f97fb9
yoshipjr <yoshihisa441396@gmail.com>
2020-01-16 18:28:38 +0900

    リスト追加機能

---
+++ src/components/ListAdd.vue
@@ -0,0 +1,34 @@
+<template> 
+  <!-- ★ここから追記 -->
+  <!-- submitボタンが押下時にaddlistというvueのメソッドを使用する -->
+  <form class="addlist" @submit.prevent="addList">
+    <!-- v-on（＠）とv-bind（：）を同時行う -->
+    <input v-model="title"
+           type="text"
+           class="text-input"
+           placeholder="Add new list"
+    >
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
+  <!-- ★ここまで追記 --> 
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: '',
+    }
+  },
+  // ★ここから追記
+  methods: {
+    addList: function() {
+      this.$store.dispatch('addlist', { title: this.title })
+      this.title = ''
+    },
+  }
+ // ★ここまで追記
+}
+</script>
